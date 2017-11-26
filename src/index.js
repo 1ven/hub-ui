7cbb54e0d9c85d763c93d495513bafc9cb85fd4d
1ven <1ven.mironchik@gmail.com>
2017-11-26 19:42:25 +0300

    Install tachyons and fonts. Add github button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => "Hello world";
+const App = () => (
+  <a
+    className="bg-blue white f6 br2 ph3 pv2 dib ma2 pointer no-underline hover-bg-black"
+    href="https://github.com/login/oauth/authorize?scope=user:email&client_id=295a8468029ab1edeadf"
+  >
+    Login with GitHub
+  </a>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
