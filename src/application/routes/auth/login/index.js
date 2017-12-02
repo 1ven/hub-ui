f4f5b4c9acfb28f88dd8f2f790e355d61efcdffc
1ven <1ven.mironchik@gmail.com>
2017-12-02 10:24:36 +0300

    Sending redirect uri

--- src/application/routes/auth/login/index.js
+++ src/application/routes/auth/login/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import join from "url-join";
 
+const { REACT_APP_API: api, REACT_APP_HOST: host } = process.env;
+
 export default () => (
   <div className="flex items-center justify-center h-100">
     <a
       className="bg-blue white f5 br2 ph4 pv3 dib pointer no-underline hover-bg-black"
-      href={join(process.env.REACT_APP_API, "/auth/github")}
+      href={join(
+        api,
+        `/auth/github?redirect_uri=${join(host, "/auth/set-token")}`
+      )}
     >
       Login with GitHub
     </a>
