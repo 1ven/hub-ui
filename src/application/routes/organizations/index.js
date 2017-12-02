de985e1c940f426f7b518239324758df0c026bb2
1ven <1ven.mironchik@gmail.com>
2017-12-02 12:02:31 +0300

    Fix linting

--- src/application/routes/organizations/index.js
+++ src/application/routes/organizations/index.js
@@ -1,4 +1,4 @@
-import { F, prop } from "ramda";
+import { prop } from "ramda";
 import { protect } from "application/data/user/hoc";
 
 export default protect(prop("isAuthenticated"))(() => "list of orgs");
