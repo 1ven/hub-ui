3345906997364a96e21aaec4ec0a35f8575f5d38
1ven <1ven.mironchik@gmail.com>
2017-12-25 19:38:17 +0300

    Change url

--- src/components/auth/pages/Login/Provider.js
+++ src/components/auth/pages/Login/Provider.js
@@ -8,6 +8,6 @@ const { REACT_APP_API: api, REACT_APP_HOST: host } = process.env;
 export default compose(
   unauthenticated,
   withProps({
-    url: join(api, `/auth/github?redirect_uri=${join(host, "/auth/set-token")}`)
+    url: join(api, `/auth?redirect_uri=${join(host, "/auth/set-token")}`)
   })
 )(View);
