ed7bc9a515217625a7a428dddebb58971f1638da
1ven <1ven.mironchik@gmail.com>
2018-01-01 17:01:56 +0300

    Use promise in fetchApi function

--- src/core/lib/redux-graphql/apiMiddleware.js
+++ src/core/lib/redux-graphql/apiMiddleware.js
@@ -3,7 +3,7 @@ import { fetchApi } from "./utils";
 import { symbol } from "./createApi";
 
 // TODO: should not be graphql specific
-export default (config = {}) => store => next => action => {
+export default (config = {}) => store => next => async action => {
   if (!action[symbol]) {
     return next(action);
   }
@@ -13,29 +13,29 @@ export default (config = {}) => store => next => action => {
     payload: action.payload
   });
 
-  fetchApi(
-    action.request,
-    mergeDeepLeft(config, action.config),
-    (body, meta) => {
-      next({
-        type: action.types.success,
-        payload: {
-          request: action.payload,
-          body,
-          meta
-        }
-      });
-    },
-    (message, body, meta) => {
-      next({
-        type: action.types.failure,
-        payload: {
-          request: action.payload,
-          message,
-          body,
-          meta
-        }
-      });
-    }
-  );
+  try {
+    const { data, meta } = await fetchApi(
+      action.request,
+      mergeDeepLeft(config, action.config)
+    );
+
+    next({
+      type: action.types.success,
+      payload: {
+        request: action.payload,
+        body: data,
+        meta
+      }
+    });
+  } catch ({ message, data, meta }) {
+    next({
+      type: action.types.failure,
+      payload: {
+        request: action.payload,
+        message,
+        data,
+        meta
+      }
+    });
+  }
 };
--- src/core/lib/redux-graphql/utils/fetchApi.js
+++ src/core/lib/redux-graphql/utils/fetchApi.js
@@ -1,29 +1,30 @@
-// graphql specific
-export default (req, { map, endpoint, headers }, onSuccess, onError) => {
-  fetch(endpoint, {
-    headers,
-    body: map.request({
-      query: req.query,
-      variables: req.variables
-    }),
-    method: "POST"
-  })
-    .then(response =>
-      response
-        .text()
-        .then(body => ({ response, body: map.response(body, response) }))
-    )
-    .then(({ response, body }) => {
+// TODO: should not be graphql specific
+export default (req, { map, endpoint, headers }) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(endpoint, {
+        headers,
+        body: map.request({
+          query: req.query,
+          variables: req.variables
+        }),
+        method: "POST"
+      });
       const meta = {
         status: response.status,
         receivedAt: Date.now()
       };
+      const body = await response.text();
 
       if (response.status >= 400) {
-        return onError(void 0, body, meta);
+        return reject({ data: body, meta });
       }
 
-      onSuccess(body.data, meta);
-    })
-    .catch(err => onError(err.message));
-};
+      resolve({
+        data: map.response(body, response).data,
+        meta
+      });
+    } catch (err) {
+      reject({ message: err.message });
+    }
+  });
